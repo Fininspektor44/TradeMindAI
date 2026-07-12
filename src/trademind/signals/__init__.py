536d5bb516d63754430a4f0e14a85c3486ff76f3
Fininspektor44 <gusev489@gmail.com>
2026-07-12 21:45:17 +0300

    feat: add signal package

---
+++ src/trademind/signals/__init__.py
@@ -0,0 +1,6 @@
+"""Signal analysis package."""
+
+from trademind.signals.engine import SignalEngine
+from trademind.signals.models import SignalAction, SignalResult
+
+__all__ = ["SignalAction", "SignalEngine", "SignalResult"]
